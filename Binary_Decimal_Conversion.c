9cd76f24649be26d6e4fac74f391ddc8987e257b
Tushar Sharma <32814311+imtusharsharma@users.noreply.github.com>
2018-06-03 23:30:30 +0530

    Add file via upload
    Program to convert Binary to Decimal

---
+++ Binary_Decimal_Conversion.c
@@ -0,0 +1,27 @@
+/*	Binary to Decimal Conversion	*/
+
+#include<stdio.h>
+
+void main()
+{	int a,b;
+		
+	printf("Enter : ");
+	
+	for(i=0;i<3;i++)
+		for(j=0;j<3;j++)
+			scanf(" %d",&A[i][j]);
+
+
+	printf("Transpose :\n");
+
+
+	for(i=0;i<3;i++)
+	{	for(j=0;j<3;j++)
+			printf(" %d",A[j][i]);
+		printf("\n");	
+	}	
+	
+	
+}	
+       
+    
