d2f6f702b8407436d6e1e4919487cc979766cb30
Tushar Sharma <32814311+imtusharsharma@users.noreply.github.com>
2018-06-03 23:38:38 +0530

    Add file via upload
    Program to Print Transpose of a Matrix

---
+++ Matrix_Transpose.c
@@ -0,0 +1,27 @@
+/*	Transpose of Matrix	*/
+
+#include<stdio.h>
+
+void main()
+{	int A[3][3], B[3][3],i=0,j=0;
+		
+	printf("Enter Matrix A : ");
+	
+	for(i=0;i<3;i++)
+		for(j=0;j<3;j++)
+			scanf(" %d",&A[i][j]);
+
+
+	printf("Transpose :\n");
+
+
+	for(i=0;i<3;i++)
+	{	for(j=0;j<3;j++)
+			printf(" %d",A[j][i]);
+		printf("\n");	
+	}	
+	
+	
+}	
+       
+    
