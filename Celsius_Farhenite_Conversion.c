927144bc154207e9fa6a98c57d1e893645666379
Tushar Sharma <32814311+imtusharsharma@users.noreply.github.com>
2018-06-04 23:21:27 +0530

    Added Files via Upload
    Program to convert Celsius to Farhenite

---
+++ Celsius_Farhenite_Conversion.c
@@ -0,0 +1,9 @@
+/* Program to Convert Celsuis to farhenite	*/
+#include<stdio.h>
+void main()
+{   float f,c;
+    printf("Enter temp in Celsius : ");
+    scanf("%f",&c); 
+    f=32+c*9/5;
+    printf("Fahrenhite = %f",f);
+}    
