e6dd1e6ba2549db9356690cfddca8aa902b822c1
Tushar Sharma <32814311+imtusharsharma@users.noreply.github.com>
2018-06-04 22:59:40 +0530

    Add files via upload
    Program to Illustrate Power Function

---
+++ Power_Function_Illustration.c
@@ -0,0 +1,21 @@
+//Power Function Illustration
+
+#include<stdio.h>
+#include<math.h>
+void main()
+{	int num,pwr;
+	float ans;
+	
+	printf("Enter Number : ");
+	scanf(" %d",&num);
+		
+	printf("\nEnter Power : ");
+	scanf(" %d",&pwr);
+	
+	ans=pow(num,pwr);	
+	
+	printf("Result %f",ans);				
+
+}	
+       
+    
